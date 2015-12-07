cf07e01b9b959c6d893fb565d0a5a439e1ec1467
kh4nhtc <KhanhTran0230@gmail.com>
2015-12-07 17:02:35 +0700

    add client's headerFactory

--- client.c
+++ client.c
@@ -3,7 +3,11 @@
 #include "cli_func/login.h"
 #include "cli_func/user_function.h"
 
+//global var
+int isMP = 0;
+
 void playGame(int sockfd);
+void headerFactory(int sockfd, SocketData s_data);
 
 int main()
 {
@@ -41,19 +45,21 @@ int main()
 
 void playGame(int sockfd){
 	SocketData s_data;
-	int isMP = 0;
+	//int isMP = 0;
 	//char* choice;
 	if(login(sockfd) == 1){
 		printf("Login success!\nWait for other player...\n");
-		s_data = readBuff(sockfd);
-		if(s_data.header == START){
+		do{
+			s_data = readBuff(sockfd);
+			headerFactory(sockfd, s_data);
+		}while(1);
+		/*if(s_data.header == START){
 			printf("%s\n",s_data.data);
 			if(strcmp(s_data.data, "Game start!\nYou are main player!")==0){
 				isMP = 1;
 			}
 			do{
-				if(isMP == 1){
-					s_data = readBuff(sockfd);
+				if(isMP == 1&&s_data.header != LOSE){
 					writeBuff(sockfd, LEVEL, selectLevel());
 				}
 				s_data = readBuff(sockfd);
@@ -64,9 +70,30 @@ void playGame(int sockfd){
 			}while(s_data.header != LOSE);
 			printf("%s\n", s_data.data);
 			return;
-		}
+		}*/
 	}else{
 		printf("Login fail!\n");
 		return;
+	}
+}
+
+void headerFactory(int sockfd, SocketData s_data){
+	switch(s_data.header){
+		case START:
+			printf("%s\n", s_data.data);
+			if(strcmp(s_data.data, "Game start!\nYou are main player!")==0){
+				isMP = 1;
+			}
+			break;
+		case LEVEL:
+			writeBuff(sockfd, LEVEL, selectLevel());
+			break;
+		case QUESTION:
+			printf("%s\n", s_data.data);
+			writeBuff(sockfd, ANSWER, answer());
+			break;
+		case LOSE:
+			printf("%s\n", s_data.data);
+			break;
 	}
 }
